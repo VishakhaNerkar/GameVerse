744b3894c7a181fba698aa733c524ca940f71648
Varun <varun@Varuns-Air.lan>
2023-02-12 22:13:37 -0800

    2nd obstacle added

---
+++ prototype_2/Assets/EnemyMovement.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float moveSpeed;
+    public Rigidbody2D Rb;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Processing inputs
+    }
+    void FixedUpdate()
+    {
+        // Physics Calculations
+    }
+}
